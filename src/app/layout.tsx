9d5f4c8d17051fa38b2ce37104150ca37786ee2f
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-27 13:57:16 +0000

    in the title of tab show the firebase logo but show the my logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <head>
         <title>Swapnil Pawar - B.Tech IT Student Portfolio</title>
         <meta name="description" content="A personal portfolio website for Swapnil Pawar to showcase projects and skills." />
-        <link rel="icon" href="https://raw.githubusercontent.com/Swapnilp011/profile-pictures/main/profile%20logo.png" />
+        <link rel="icon" href="https://github.com/Swapnilp011/profile-pictures/blob/main/profile%20logo.png?raw=true" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
